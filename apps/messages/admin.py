ae615f046409cfde62ec6ec8142912b7e6d68f0f
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-05-05 12:35:49 +0500

    permissions and filters

--- apps/messages/admin.py
+++ apps/messages/admin.py
@@ -1,4 +1,17 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
+
 from .models import Message
 
-admin.site.register(Message)
+
+@register(Message)
+class MessageAdmin(ModelAdmin):
+    list_display = ("id", "author", "channel", "short_content", "created_at", "updated_at")
+    list_filter = ("channel", "author", "created_at")
+    search_fields = ("content", "author__email", "author__first_name", "author__last_name")
+    raw_id_fields = ("author", "channel", "parent_message")
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("-created_at",)
+
+    def short_content(self, obj):
+        return obj.content[:60] + "..." if len(obj.content) > 60 else obj.content
+    short_content.short_description = "Content"
