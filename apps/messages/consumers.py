9e5d1112e06caae0d99cda94eb6ddc5cc7605732
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-05-06 18:43:59 +0500

    async code (i/o bound)

--- apps/messages/consumers.py
+++ apps/messages/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .async_utils import fetch_message_stats_async, process_message_async
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -9,10 +10,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = f"chat_{self.channel_id}"
 
         # need to comment this out for now since we don't have auth set up yet, but in a real app you'd want to check if the user is authenticated before allowing them to connect
-        user = self.scope["user"] 
-        if not user.is_authenticated:
-            await self.close()
-            return
+        # user = self.scope["user"] 
+        # if not user.is_authenticated:
+        #     await self.close()
+        #     return
         #-------
 
         await self.channel_layer.group_add(
@@ -22,9 +23,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # send connection confirmation + channel stats to the joining client
+        stats = await fetch_message_stats_async(self.channel_id)
         await self.send(text_data=json.dumps({
             "type": "connection_established",
             "message": "You're now connected",
+            "stats": stats,
         }))
 
     async def disconnect(self, _close_code):
@@ -36,14 +40,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         """Called automatically when the client sends a message."""
         data = json.loads(text_data)
-        message = data["message"]
+        raw_content = data.get("message", "")
+
+        # process (spam check + clean) before broadcasting
+        result = await process_message_async(raw_content)
+        if not result["ok"]:
+            await self.send(text_data=json.dumps({
+                "type": "error",
+                "message": result["reason"],
+            }))
+            return
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
-                "message": message,
-                "sender": self.scope["user"].email,
+                "message": result["content"],
+                "sender": getattr(self.scope["user"], "email", "anonymous"),
             },
         )
 
