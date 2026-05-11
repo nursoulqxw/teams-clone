b05a380a8360a6257024760cc04550efb441e293
akoncore <akniet.talgat.05@mail.ru>
2026-05-11 23:51:56 +0500

    added celery and updated settings/base.py and celery.py

--- settings/celery.py
+++ settings/celery.py
@@ -1,11 +1,11 @@
 import os
-
+from django.conf import settings
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.env.local')
 
-app = Celery('settings')
+app = Celery('teams')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -14,9 +14,7 @@ app = Celery('settings')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+app.conf.timezone = 'UTC'
 
-# @app.task(bind=True, ignore_result=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
