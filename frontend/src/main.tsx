8327149a2adeec951a44e275e3ce72e064e1287a
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-05-15 00:43:12 +0500

    feat(frontend): wrap app with LangProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App";
+import { LangProvider } from "./i18n/LangContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,7 +16,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <LangProvider>
+          <App />
+        </LangProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
